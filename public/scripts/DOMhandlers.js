704533703d38edfd7163bca82a6596e9a2566f35
frayfray248 <nicman248@hotmail.com>
2023-04-07 15:50:34 -0700

    can't send empty message

--- public/scripts/DOMhandlers.js
+++ public/scripts/DOMhandlers.js
@@ -22,11 +22,13 @@ const registerDOMHandlers = (socket) => {
     const sendMessageHandler = (event) => {
         event.preventDefault()
 
+        const message = $('#messageInput').val()
+
+        if (!message) return
+
         const socket = event.data.socket
         const username = localStorage.getItem('username')
         const key = localStorage.getItem('roomKey')
-
-        const message = $('#messageInput').val()
 
         socket.emit('sendMessage', message, username, key)
         $('#messageInput').val("")
